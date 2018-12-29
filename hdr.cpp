f8cc99689ba1168387874f4f13657eaa3be3f4f0
yumingchang <yumingchang8587@gmail.com>
2018-12-29 18:48:55 +0800

    read image done + small image read

--- hdr.cpp
+++ hdr.cpp
@@ -18,7 +18,7 @@
 using namespace std;
 using namespace cv;
 
-void load_exposures( string source_dir, uint8_t **img_list_b, uint8_t **img_list_g, uint8_t **img_list_r, uint8_t **small_b, uint8_t **small_g, uint8_t **small_r, int *exposure_log2, unsigned int *row_input, unsigned int *col_input, int pic_count ){
+void load_exposures( string source_dir, uint8_t **img_list_b, uint8_t **img_list_g, uint8_t **img_list_r, uint8_t **small_b, uint8_t **small_g, uint8_t **small_r, int **exposure_log2, unsigned int *row_input, unsigned int *col_input, int pic_count ){
 	fstream txt;
 	txt.open( source_dir + "/image_list.txt", fstream::in );
 	if( !txt ){
@@ -36,9 +36,6 @@ void load_exposures( string source_dir, uint8_t **img_list_b, uint8_t **img_list
 				cout << temp << endl;
 				Mat input_pic = imread( temp, CV_LOAD_IMAGE_COLOR);
 				if( input_pic.data ){
-					vector<Mat> channels;
-					split( input_pic, channels );
-					//cout << channels[0] << endl;
 					if( ( *row_input ) == 0 && ( *col_input ) == 0 ){
 						cout << "allocating area for input pictures" << pic_count << endl;
 						( *row_input ) = input_pic.rows;
@@ -47,20 +44,35 @@ void load_exposures( string source_dir, uint8_t **img_list_b, uint8_t **img_list
 						*img_list_b = new uint8_t[ ( *row_input ) * ( *col_input ) * pic_count ];
 						*img_list_g = new uint8_t[ ( *row_input ) * ( *col_input ) * pic_count ];
 						*img_list_r = new uint8_t[ ( *row_input ) * ( *col_input ) * pic_count ];
-						// *small_b = new uint8_t[ SMALLPIXELS * pic_count ];
-						// *small_g = new uint8_t[ SMALLPIXELS * pic_count ];
-						// *small_r = new uint8_t[ SMALLPIXELS * pic_count ];
+						*small_b = new uint8_t[ SMALLPIXELS * pic_count ];
+						*small_g = new uint8_t[ SMALLPIXELS * pic_count ];
+						*small_r = new uint8_t[ SMALLPIXELS * pic_count ];
+						*exposure_log2 = new int[ pic_count ];
 						//cout << "Picture size is : " << distance << " " << channels[0].total() << endl;
 					}
 					cout << "Reading " << pointer + 1 << "th picture to memory" << endl;
+
+					// saving picture by channel
+					vector<Mat> channels;
+					split( input_pic, channels );
 					unsigned offset = pointer * distance;
 					memcpy( ( *img_list_b ) + offset, channels[0].data, channels[0].total() );
 					memcpy( ( *img_list_g ) + offset, channels[1].data, channels[1].total() );
 					memcpy( ( *img_list_r ) + offset, channels[2].data, channels[2].total() );
-					cout << distance << endl;
-					for( unsigned i = offset ; i < distance + offset ; ++i ){
-						cout << (int)( *img_list_b )[i] << " " << (int)( *img_list_g )[i] << " " << (int)( *img_list_r )[i] << " " << endl;
-					}
+
+					// getting 10 * 10 resized image from original image
+					Mat small;
+					resize( input_pic, small, cv::Size( 10, 10 ), 0, 0 );
+					vector<Mat> small_channels;
+					split( small, small_channels );
+					offset = pointer * SMALLPIXELS;
+					memcpy( ( *small_b ) + offset, small_channels[0].data, small_channels[0].total() );
+					memcpy( ( *small_g ) + offset, small_channels[1].data, small_channels[1].total() );
+					memcpy( ( *small_r ) + offset, small_channels[2].data, small_channels[2].total() );
+
+					//for( unsigned i = offset ; i < distance + offset ; ++i ){
+					//	cout << (int)( *img_list_b )[i] << " " << (int)( *img_list_g )[i] << " " << (int)( *img_list_r )[i] << " " << endl;
+					//}
 					pointer++;
 				}
 			}
@@ -90,15 +102,15 @@ int main( int argc, char* argv[] ){
 	cout << "reading input images ... " << endl;
 
 	row = col = 0;
-	load_exposures( img_dir, &img_list_b, &img_list_g, &img_list_r, &small_b, &small_g, &small_r, exposure_log2, &row, &col, pic_count );
+	load_exposures( img_dir, &img_list_b, &img_list_g, &img_list_r, &small_b, &small_g, &small_r, &exposure_log2, &row, &col, pic_count );
 
-	//delete[] img_list_b;
-	//delete[] img_list_g;
-	//delete[] img_list_r;
+	delete[] img_list_b;
+	delete[] img_list_g;
+	delete[] img_list_r;
 
-	//delete[] small_b;
-	//delete[] small_g;
-	//delete[] small_r;
+	delete[] small_b;
+	delete[] small_g;
+	delete[] small_r;
 
-	//delete[] exposure_log2;
+	delete[] exposure_log2;
 }
